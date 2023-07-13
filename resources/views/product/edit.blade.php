b4d895b3951c8987fe9bca85b7f63e6fd2c11764
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-07-13 07:39:35 +0700

    update onclick

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -42,7 +42,7 @@
                         </div>
                         <div class="flex justify-end">
                             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Save</button>
-                            <button type="button" onclick="closeeditProductModal()" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">Close</button>
+                            <button type="button" onclick="closeEditProductModal()" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">Close</button>
                         </div>
                     </form>
                 </div>
